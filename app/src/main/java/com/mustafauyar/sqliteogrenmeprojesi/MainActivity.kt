cfb736e5559dffb5ce22914e5cf06dae8f2ba9d5
Mustafa2 <Mustafauyar91@gmail.com>
2024-02-04 15:08:01 +0300

    Sqlite execute komutlari veri ekleme ve alma

--- app/src/main/java/com/mustafauyar/sqliteogrenmeprojesi/MainActivity.kt
+++ app/src/main/java/com/mustafauyar/sqliteogrenmeprojesi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mustafauyar.sqliteogrenmeprojesi
 import com.mustafauyar.sqliteogrenmeprojesi.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,10 +12,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         try {
             val db = this.openOrCreateDatabase("Urunler", MODE_PRIVATE,null)
-            // pythonda rastladığım te tırnak sorununu karşılaşmamak için üç trınak rasında kullandım execute u
-            db.execSQL("""CREATE TABLE IF NOT EXISTS urunler(id INTIGER PRIMARY KEY,isim VARCHAR,fiyat INT)""")
-            db.execSQL("""INSERT INTO urunler (isim, fiyat) VALUES ("Ayakkabı",100)""")
+            // pythonda rastladığım tek tırnak sorununu karşılaşmamak için üç tırınak arasında kullandım execute u
+            db.execSQL("DROP TABLE IF EXISTS urunler")// veri tabanı table sil
+            // her seferinde baştan oluşturmak için urunler tablosunu sil
 
+            db.execSQL("""CREATE TABLE IF NOT EXISTS urunler(id INTEGER PRIMARY KEY,isim VARCHAR,fiyat INT)""")
+            // tablo oluştur
+            val myShop = HashMap<String,Int>() // ürünler sözlüğü
+            myShop.put("Ayakkabi",100)
+            myShop["Takke"] = 10
+            myShop["gozluk"] = 750
+            myShop["saat"] = 87
+            myShop.forEach { (k, v) ->
+                Log.d("Cursor dict", "$k : $v")
+                db.execSQL("""INSERT INTO urunler (isim, fiyat) VALUES ('$k',$v)""") // isimler belirtirken tek tırnak kullanılmalıdır
+                // her bir key ve value leri tabloya kaydet Cursor dict etiketi ile logcat de göster
+            }
+//            db.execSQL("""INSERT INTO urunler (isim, fiyat) VALUES ("Ayakkabı",100)""")
+//            db.execSQL("""INSERT INTO urunler (isim, fiyat) VALUES ("Takke",10)""")
+//            db.execSQL("""INSERT INTO urunler (isim, fiyat) VALUES ("Gozluk",750)""")
+
+
+            val cursor = db.rawQuery("SELECT * FROM urunler",null)
+            val idColumIndex = cursor.getColumnIndex("id")
+            val nameColumnIndex = cursor.getColumnIndex("isim")
+            val priceColumnIndex = cursor.getColumnIndex("fiyat")
+            val blockTag = "Cursor ürünler"
+            while (cursor.moveToNext()){
+                Log.d(blockTag, "ID : ${cursor.getInt(idColumIndex)}")
+                Log.d(blockTag,"İSİM : ${cursor.getString(nameColumnIndex)}")
+                Log.d(blockTag,"FİYAT : ${cursor.getInt(priceColumnIndex)}")
+            }
+            cursor.close()
+//            db.execSQL("DROP TABLE urunler")// veri tabanı table sil
         } catch (e:Exception){
             e.printStackTrace()
         }
